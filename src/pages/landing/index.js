c1741cb189ea3d96143f3cfceeb9ee46efcb0ca6
arul14444 <mochamadfachrul14@gmail.com>
2025-05-14 16:53:48 +0700

    Add card components

--- src/pages/landing/index.js
+++ src/pages/landing/index.js
@@ -76,30 +76,74 @@ let Body = () => {
                 <div className="col-4">
                 trusted by top teams
                 </div>
-                <div className="col-1 trustTeamLogo">
+                <div className="trustTeamLogo col-1 ">
                     <img src="https://www.notion.com/_next/image?url=https%3A%2F%2Fimages.ctfassets.net%2Fspoqsaf9291f%2FjWkCPMAAj0Av69704DQoB%2F84499b606028b276e0bf94c238f07097%2FOpenAI-black-wordmark-cropped.png&w=3840&q=75" alt="OpenAI Logo" className="imageCol4" />
                 </div>
-                <div className="col-1 trustTeamLogo">
+                <div className="trustTeamLogo col-1">
                     <img src="https://images.ctfassets.net/spoqsaf9291f/502ApiNcRHgIwrDU8XRYTQ/497492503acb3fa21bbd9dd1aec193c3/Figma-logo-color.svg" alt="Figma Logo" className="imageCol4" />
                 </div>
-                <div className="col-1 trustTeamLogo">
+                <div className="trustTeamLogo col-1">
                     <img src="https://www.notion.com/_next/image?url=https%3A%2F%2Fimages.ctfassets.net%2Fspoqsaf9291f%2F4xtSoCGKxiZcTwsYPlBB5h%2F0fb7d4155e89ef5a852936099e014313%2Fvolvo.png&w=1920&q=75" alt="Volvo Logo" className="imageCol4" />
                 </div>
-                <div className="col-1 trustTeamLogo">
+                <div className="trustTeamLogo col-1">
                     <img src="https://www.notion.com/_next/image?url=https%3A%2F%2Fimages.ctfassets.net%2Fspoqsaf9291f%2F4eiEZBWL5gEClwB1IHi18I%2Fb9b86479965977121ceabd01f45567a7%2Framp.png&w=3840&q=75" alt="Ramp Logo" className="imageCol4" />
                 </div>
-                <div className="col-1 trustTeamLogo">
+                <div className="trustTeamLogo col-1">
                     <img src="https://www.notion.com/_next/image?url=https%3A%2F%2Fimages.ctfassets.net%2Fspoqsaf9291f%2F3CSWr4Z4uIvbfO7102KPY%2Fbbd97c8edd0673853ac8dae2887a638d%2Fcursor.png&w=1920&q=75" alt="Cursor Logo" className="imageCol4" />
                 </div>
-                <div className="col-1 trustTeamLogo">
+                <div className="trustTeamLogo col-1">
                     <img src="https://images.ctfassets.net/spoqsaf9291f/xDZNk5XtOxMEiqsiJ6paq/5264c929774566fc6f02fbb47e7aea97/Headspace-logo-color.svg" alt="Headspace Logo" className="imageCol4" />
                 </div>
-                <div className="col-1 trustTeamLogo">
+                <div className="trustTeamLogo col-1">
                     <img src="https://images.ctfassets.net/spoqsaf9291f/3hzRKZTT0Mi4Q3JLPQpQyF/28256f50d0bf6b63bfbed7862b8cbe68/Perplexity_AI_logo.svg.png" alt="Perplexity AI Logo" className="imageCol4" />
                 </div>
-                <div className="col-1 trustTeamLogo">
+                <div className="trustTeamLogo col-1">
                     <img src="https://images.ctfassets.net/spoqsaf9291f/15en0a60cYTRxNO1QlHbdg/f09657a6b8287f1db0e628a048267e2c/vercel-logotype-light.svg" alt="Vercel Logo" className="imageCol4" />
                 </div>
+            </div>
+
+            <div className="row">
+            <div className="col-sm-6">
+                <div className="redCard card">
+                <div className="card-body">
+                    <p>
+                        AI Meeting Notes
+                    </p>
+                    <h5 className="card-title">Perfect notes every time.</h5>
+                    <img>
+
+                    </img>
+                </div>
+                </div>
+            </div>
+            <div className="col-sm-6">
+                <div className="blueCard card">
+                <div className="card-body">
+                    <p>
+                        Enterprise Search
+                    </p>
+                    <h5 className="card-title">One search for everything</h5>
+                    <img>
+
+                    </img>
+                </div>
+                </div>
+            </div>
+            </div>
+            <div className="row">
+            <div className="col-sm-12">
+                <div className="yellowCard card">
+                <div className="card-body">
+                    <p>
+                        projects
+                    </p>
+                    <h5 className="card-title">Keep every plan on track</h5>
+                    <img>
+
+                    </img>
+                </div>
+                </div>
+            </div>
             </div>
         </div>
     )
